4fcfb935c0ba9184da7a79c407876c54b274c696
Alessandro Frisone <alefri2004@gmail.com>
2025-04-28 18:23:26 +0200

    feat: snacks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,63 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 
-export default function App() {
-  const [politicians, setPoliticians] = useState([])
+function PoliticianCard({ name, image, position, biography }) {
+
+
+
+  return (
+    <div className="card">
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <p>
+        <strong>Posizione: {position}</strong>
+      </p>
+      <p>{biography}</p>
+    </div>
+  );
 }
 
+const MemoizedPoliticinaCard = React.memo(PoliticianCard)
+
+export default function App() {
+  const [politicians, setPoliticians] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/politicians`)
+      .then((res) => res.json())
+      .then((data) => setPoliticians(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter((politician) => {
+      const isInName = politician.name
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const isInDesc = politician.description
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      return isInName || isInDesc;
+    });
+  }, [politicians, search]);
+
+  return (
+    <div>
+      <h1>Lista politici</h1>
+      <input
+        type="text"
+        placeholder="Cerca per nome o biografia"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="politicinas">
+        {filteredPoliticians.map((politician) => (
+          <MemoizedPoliticinaCard key={politician.id} {...politician} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 // 📌 Milestone 1: Recuperare e visualizzare i dati
 // Effettua una chiamata API a
